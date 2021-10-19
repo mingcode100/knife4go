9ba9152486977752100c3c088f07148fc8c00b77
fanbeibei <fanbeibei@linklogs.com>
2021-10-19 16:50:32 +0800

    图片

--- knife4go-example/main.go
+++ knife4go-example/main.go
@@ -5,13 +5,33 @@ import (
 	gin_swagger_knife "gitee.com/youbeiwuhuan/knife4go/gin-swagger-knife"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 func main() {
 	router := gin.Default()
-
+	// 获取 swag int 命令生成的swagger.json文件里的内容
 	swaggerJson := getFileContent("./swagger.json")
+	fmt.Println(swaggerJson)
 	gin_swagger_knife.InitSwaggerKnife(router,swaggerJson)
+	router.GET("/hello",func(ctx *gin.Context) {
+		rs := []byte("dafafdasfaf")
+
+		ctx.Status(http.StatusOK)
+		ctx.Header("content-type", "application/json;charset=UTF-8")
+		ctx.Header("content-length", strconv.Itoa(len(rs)))
+		ctx.Header("connection", "keep-alive")
+
+		_, err := ctx.Writer.Write(rs)
+		if nil != err {
+			log.Fatal(err)
+			return
+		}
+
+		ctx.Writer.Flush()
+	})
 
 	router.Run(":8080")
 }
