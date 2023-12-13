67a257310f32544f040f7d96fc2409ec8c01edad
fanbeibei <fanbeibei@linklogs.com>
2023-12-13 10:46:28 +0800

    删掉

---
+++ gin-openapi3-knife-example/README.md
@@ -0,0 +1,6 @@
+# knife4go-openapi3-example
+
+#### 介绍
+knife4go使用示例
+
+
---
+++ gin-openapi3-knife-example/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	gin_openapi3_knife "gitee.com/youbeiwuhuan/knife4go/gin-openapi3-knife"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+	router := gin.Default()
+	// 获取 swag int 命令生成的swagger.json文件里的内容
+	//swaggerJson := getFileContent("./swagger.json")
+	swaggerJson := getFileContent("./open-api-v3.json")
+	fmt.Println(swaggerJson)
+	gin_openapi3_knife.InitSwaggerKnife(router, swaggerJson)
+	router.GET("/hello", func(ctx *gin.Context) {
+		rs := []byte("dafafdasfaf")
+
+		ctx.Status(http.StatusOK)
+		ctx.Header("content-type", "application/json;charset=UTF-8")
+		ctx.Header("content-length", strconv.Itoa(len(rs)))
+		ctx.Header("connection", "keep-alive")
+
+		_, err := ctx.Writer.Write(rs)
+		if nil != err {
+			log.Fatal(err)
+			return
+		}
+
+		ctx.Writer.Flush()
+	})
+
+	router.Run(":8080")
+}
+
+func getFileContent(fpath string) string {
+	bytes, err := ioutil.ReadFile(fpath)
+	if nil != err {
+		fmt.Errorf(" %s getFileBase64 error: %v", fpath, err)
+		return ""
+	}
+
+	return string(bytes)
+}
